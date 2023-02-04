b47ff367c4d1cf7b9225281c9f8a55c85255d152
idlakshan <id.lakshan21@gmail.com>
2023-02-04 13:52:28 +0530

    member form updated

--- src/controller/MemberController.java
+++ src/controller/MemberController.java
@@ -2,19 +2,134 @@ package controller;
 
 import model.Member;
 
+import java.sql.*;
+
 public class MemberController {
-    public static boolean addMember(){
+
+    public static boolean addMember(Member member){
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library",
+                    "root", "1234");
+
+            PreparedStatement statement = connection.prepareStatement("insert into member values(?,?,?,?,?)");
+
+            statement.setObject(1,member.getMid());
+            statement.setObject(2,member.getName());
+            statement.setObject(3,member.getAddress());
+            statement.setObject(4,member.getEmail());
+            statement.setObject(5,member.getTel());
+
+            int i = statement.executeUpdate();
+
+            if (i>0){
+                return true;
+            }else {
+                return false;
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean updateMember(Member member){
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library",
+                    "root", "1234");
+
+            PreparedStatement statement = connection.prepareStatement("update member set name=?,address=?,email=?,tel=? where mid=?");
+
+            statement.setObject(1,member.getName());
+            statement.setObject(2,member.getAddress());
+            statement.setObject(3,member.getEmail());
+            statement.setObject(4,member.getTel());
+            statement.setObject(5,member.getMid());
+
+            int i = statement.executeUpdate();
+
+            if (i>0){
+                return true;
+            }else {
+                return false;
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return false;
     }
-    public static boolean updateMember(){
+
+
+    public static boolean deleteMember(String deleteId){
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library",
+                    "root", "1234");
+            PreparedStatement statement = connection.prepareStatement("delete from member where mid =?");
+
+            statement.setObject(1,deleteId);
+
+            int i = statement.executeUpdate();
+
+            if (i>0){
+                return true;
+            }else {
+                return false;
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return false;
     }
-    public static boolean deleteMember(){
-        return false;
-    }
-    public static Member searchMember(){
-        return null;
+
+
+    public static Member searchMember(String searchId){
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library",
+                    "root", "1234");
+            PreparedStatement statement = connection.prepareStatement("select * from member where mid=?");
+
+            statement.setObject(1,searchId);
+            ResultSet resultSet = statement.executeQuery();
+
+            Member member=new Member();
+
+            if (resultSet.next()){
+                member.setMid(resultSet.getString(1));
+                member.setName(resultSet.getString(2));
+                member.setAddress(resultSet.getString(3));
+                member.setEmail(resultSet.getString(4));
+                member.setTel(resultSet.getString(5));
+
+                return member;
+
+            }
+
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+       return null;
     }
 
 }
--- src/view/MemberFormController.java
+++ src/view/MemberFormController.java
@@ -1,11 +1,16 @@
 package view;
 
 import com.jfoenix.controls.JFXTextField;
+import controller.MemberController;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import model.Member;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class MemberFormController {
 
@@ -24,7 +29,7 @@ public class MemberFormController {
         String email = txtEmail.getText();
         String tel = txtTel.getText();
 
-        try {
+        /*try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library",
                     "root", "1234");
@@ -40,6 +45,7 @@ public class MemberFormController {
 
             if (i>0){
                 new Alert(Alert.AlertType.INFORMATION,"Member has been saved").show();
+                clearTextFields();
             }else {
                 new Alert(Alert.AlertType.CONFIRMATION,"Save Member was failed").show();
             }
@@ -49,16 +55,114 @@ public class MemberFormController {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
+        }*/
+
+        Member member=new Member();
+        member.setMid(id);
+        member.setName(name);
+        member.setAddress(address);
+        member.setEmail(email);
+        member.setTel(tel);
+
+        boolean isAdded = MemberController.addMember(member);
+
+        if (isAdded){
+            new Alert(Alert.AlertType.INFORMATION,"Member has been saved").show();
+            clearTextFields();
+        }else {
+            new Alert(Alert.AlertType.CONFIRMATION,"Save Member was failed").show();
         }
 
     }
 
     public void UpdateMember(ActionEvent event) {
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        String email = txtEmail.getText();
+        String tel = txtTel.getText();
 
+       /* try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library",
+                    "root", "1234");
+
+            PreparedStatement statement = connection.prepareStatement("update member set name=?,address=?,email=?,tel=? where mid=?");
+
+            statement.setObject(1,name);
+            statement.setObject(2,address);
+            statement.setObject(3,email);
+            statement.setObject(4,tel);
+            statement.setObject(5,id);
+
+            int i = statement.executeUpdate();
+
+            if (i>0){
+                new Alert(Alert.AlertType.INFORMATION,"Member has been Updated").show();
+                clearTextFields();
+            }else{
+                new Alert(Alert.AlertType.CONFIRMATION,"Update failed").show();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+*/
+
+        Member member=new Member();
+        member.setMid(id);
+        member.setName(name);
+        member.setAddress(address);
+        member.setEmail(email);
+        member.setTel(tel);
+
+        boolean isUpdated = MemberController.updateMember(member);
+
+
+        if (isUpdated){
+            new Alert(Alert.AlertType.INFORMATION,"Member has been Updated").show();
+            clearTextFields();
+        }
     }
 
     public void DeleteMember(ActionEvent event) {
 
+        String deleteId = txtSearchId.getText();
+
+        boolean isDeleted = MemberController.deleteMember(deleteId);
+
+        if (isDeleted){
+            new Alert(Alert.AlertType.INFORMATION,"Member has been Deleted").show();
+            clearTextFields();
+        }else{
+            new Alert(Alert.AlertType.CONFIRMATION,"There is no Member such as a given number").show();
+        }
+
+       /* try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library",
+                    "root", "1234");
+
+            PreparedStatement statement = connection.prepareStatement("delete from member where mid =?");
+            statement.setObject(1,deleteId);
+
+            int i = statement.executeUpdate();
+
+            if (i>0){
+                new Alert(Alert.AlertType.INFORMATION,"Member has been Deleted").show();
+                clearTextFields();
+
+            }else {
+                new Alert(Alert.AlertType.CONFIRMATION,"There is no Member such as a given number").show();
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+*/
     }
 
     public void GetAll(ActionEvent event) {
@@ -66,7 +170,8 @@ public class MemberFormController {
     }
 
     public void SearchMember(ActionEvent event) {
-        String searchId = txtSearchId.getText();
+
+       /* String searchId = txtSearchId.getText();
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -79,15 +184,17 @@ public class MemberFormController {
             ResultSet resultSet = statement.executeQuery();
 
             if (resultSet.next()){
-                new Alert(Alert.AlertType.INFORMATION,"Member search successfully done").show();
 
               txtId.setText(resultSet.getString(1));
               txtName.setText(resultSet.getString(2));
               txtAddress.setText(resultSet.getString(3));
               txtEmail.setText(resultSet.getString(4));
               txtTel.setText(resultSet.getString(5));
+
+              txtSearchId.clear();
+
             }else {
-                new Alert(Alert.AlertType.CONFIRMATION,"There is no Member").show();
+                new Alert(Alert.AlertType.CONFIRMATION,"There is no Member such as a given number").show();
             }
 
 
@@ -96,6 +203,26 @@ public class MemberFormController {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
-        }
+        }*/
+
+        String searchId = txtSearchId.getText();
+
+        Member member = MemberController.searchMember(searchId);
+
+        txtId.setText(member.getMid());
+        txtName.setText(member.getName());
+        txtAddress.setText(member.getAddress());
+        txtEmail.setText(member.getEmail());
+        txtTel.setText(member.getTel());
+
+    }
+
+
+    public void clearTextFields(){
+        txtId.setText("");
+        txtName.setText("");
+        txtAddress.setText("");
+        txtEmail.setText("");
+        txtTel.setText("");
     }
 }
